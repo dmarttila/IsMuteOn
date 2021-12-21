2081612a917eafb82620bd9a57c6a4ceba411121
Doug Marttila <dmarttila@gmail.com>
2021-12-21 14:11:39 -0500

    Initial Commit

---
+++ IsMuteOn/IsMuteOnApp.swift
@@ -0,0 +1,17 @@
+//
+//  IsMuteOnApp.swift
+//  IsMuteOn
+//
+//  Created by Douglas Marttila on 12/21/21.
+//
+
+import SwiftUI
+
+@main
+struct IsMuteOnApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
