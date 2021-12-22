f8a7a9581e8411441455b79d9345b599fc5966cf
Doug Marttila <dmarttila@gmail.com>
2021-12-22 12:11:58 -0500

    Initial commit. Most of this was figured out in Bready, but that didn't seem like the best way to show this one feature. So not much of a commit history here.

---
+++ IsMuteOn/CheckMute.swift
@@ -0,0 +1,68 @@
+//
+//  CheckMute.swift
+//  IsMuteOn
+//
+//  Created by Doug Marttila on 12/22/21.
+//  System sound creation and playing from: https://www.rockhoppertech.com/blog/apple-system-sounds/
+//  Mute check idea from: https://github.com/akramhussein/Mute
+//  Play a 1/10th second of silence and time how long it takes. If it takes less than 1/10th second, mute is on. (If the mute button is on when a system sound is triggered iOS doesn't play anything.) This only works for system sounds. AVAudioPlayer will play a sound silently if mute is on.
+//  How this can fail: The user could change the mute setting while the silent sound is playing (It's easier to observe this failure if you use the bell sound rather than the oneTenthSecond sound)
+//  Will Apple approve this? Don't know
+//  Doesn't seem to be an easy way to simulate mute in the simulator, so you need to use a device
+
+import AVFoundation
+
+class CheckMute {
+    //singleton which actually feels like the best solution in this case
+    public static let shared = CheckMute()
+    private init () {}
+    //optional in case the value is requested before the test completes
+    public var muteIsOn: Bool?
+    
+    private var timeStamp = 0.0
+    private var silence: SystemSoundID = .zero
+    private var isPlayingSound = false
+    
+    //callBack tells the caller that there is a result for the "is the mute button on" test
+    //You could just have a single callback, but there could be multiple calls to this test from different views. If so, you'd want all the views to get the result, but there's no need to continually trigger the sound to play if the multiple calls happen before the sound stops playing
+    private var callBacks = [() -> Void] ()
+    
+    public func checkForMute (callBack: @escaping () -> ()) {
+        callBacks.append(callBack)
+        if isPlayingSound { return }
+        isPlayingSound = true
+        //create the sound id if it doesn't already exist
+        if silence == .zero {
+            silence = createSysSound(fileName: "oneTenthSecond", fileExt: "mp3")
+        }
+        timeStamp = Date().timeIntervalSinceReferenceDate
+        AudioServicesPlaySystemSoundWithCompletion(silence) { [weak self] in
+            self?.soundFinishedPlaying()
+        }
+    }
+    
+    private func createSysSound(fileName: String, fileExt: String) -> SystemSoundID {
+        var mySysSound: SystemSoundID = .zero
+        guard let url = Bundle.main.url(forResource: fileName, withExtension: fileExt) else {
+            return .zero
+        }
+        let osstatus = AudioServicesCreateSystemSoundID(url as CFURL, &mySysSound)
+        if osstatus != noErr { // or kAudioServicesNoError. same thing.
+            print("could not create system sound")
+            print("osstatus: \(osstatus)")
+        }
+        return mySysSound
+    }
+    
+    private func soundFinishedPlaying() {
+        isPlayingSound = false
+        let timeElapsed = Date().timeIntervalSinceReferenceDate - timeStamp
+        print(callBacks.count)
+        print("done playing sound \(timeElapsed)")
+        muteIsOn = timeElapsed < 0.05
+        while callBacks.count > 0 {
+            let cb = callBacks.removeFirst()
+            cb()
+        }
+    }
+}
--- IsMuteOn/ContentView.swift
+++ IsMuteOn/ContentView.swift
@@ -2,15 +2,50 @@
 //  ContentView.swift
 //  IsMuteOn
 //
-//  Created by Douglas Marttila on 12/21/21.
+//  Created by Doug Marttila on 12/21/21.
 //
 
 import SwiftUI
 
 struct ContentView: View {
+    
+    func checkMute1 () {
+        CheckMute.shared.checkForMute {
+            isMuted = CheckMute.shared.muteIsOn
+            mutedStr1 = isMuted! ? "Device is muted" : "Device isn't muted"
+            print("Call back in Content View 1")
+        }
+    }
+    func checkMute2 () {
+        CheckMute.shared.checkForMute {
+            isMuted = CheckMute.shared.muteIsOn
+            mutedStr2 = isMuted! ? "Device is muted" : "Device isn't muted"
+            print("Call back in Content View 2")
+        }
+    }
+
+    @State private var isMuted: Bool? = nil
+    @State private var mutedStr1 = "-"
+    @State private var mutedStr2 = "-"
+
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        VStack {
+            Text("Two checks to make sure the callback comes to both views if they're both clicked")
+            Divider()
+            HStack {
+                Text(mutedStr1)
+                Button("Check mute 1") {
+                    checkMute1()
+                }
+            }
+            Divider()
+            HStack {
+                Text(mutedStr2)
+                Button("Check mute 2") {
+                    checkMute2()
+                }
+            }
+        }
     }
 }
 
